38169a202a461a0c0ecc1e897db68d362f1ff6b2
Ferruccio Balestreri <ferruccio.balestreri@gmail.com>
2024-04-14 12:58:37 +0200

    Export styles separately from the components (ideally we'll only export relevant styles for each components)

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       formats: ['es'],
     },
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime'],
+      external: ['react', 'react/jsx-runtime', "tailwindcss"],
       output: {
         globals: {
           tailwindcss: "tailwindcss"
