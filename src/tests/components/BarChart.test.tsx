60a362d5996c93275e4770f0157cdc5ec8d1fe72
Ferruccio Balestreri <ferruccio.balestreri@gmail.com>
2024-04-14 22:42:41 +0200

    Add more test cases

--- src/tests/components/BarChart.test.tsx
+++ src/tests/components/BarChart.test.tsx
@@ -1,3 +1,5 @@
+
+import userEvent from '@testing-library/user-event';
 import { render, screen } from '../test-utils';
 import { BarChart } from '../../../lib/components/BarChart';
 
@@ -16,6 +18,20 @@ describe('BarChart', () => {
     const noDataText = "No data available";
     render(<BarChart categories={['Power users']} data={[]} index="month" noDataText={noDataText} />);
     expect(await screen.findByText(noDataText)).toBeInTheDocument();
+  });
+
+  it('tooltips are visible on hover', async () => {
+    const { container } = render(<BarChart data={mockData} categories={['Sales']} index="month" />);
+    const user = userEvent.setup();
+    const element = container.querySelector('recharts-tooltip-wrapper');
+
+    user.hover(element as Element).then(() => {
+      const tooltipElements = container.querySelectorAll('recharts-tooltip-wrapper');
+
+      tooltipElements.forEach((tooltipElement) => {
+        expect(tooltipElement).toBeVisible();
+      });
+    });
   });
 });
 
