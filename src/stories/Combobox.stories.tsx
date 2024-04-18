4261b9cc5e6370cb965787d6c25a6f3dbeb610cc
Ferruccio Balestreri <ferruccio.balestreri@gmail.com>
2024-04-18 11:13:45 +0200

    Add story for long option name

--- src/stories/Combobox.stories.tsx
+++ src/stories/Combobox.stories.tsx
@@ -48,6 +48,48 @@ export const Default: Story = {
   </Popover>;
   }
 };
+type OptionType = { value: string; label: string };
 
+type VeryLongOptionStory = StoryObj<{
+  args: {
+    customOptions: OptionType[];
+  };
+  argTypes: {
+    customOptions: {
+      control: string;
+      description: string;
+    };
+  };
+  render: (args: { customOptions: OptionType[] }) => JSX.Element;
+}>;
 
-
+export const VeryLongOption: VeryLongOptionStory = {
+  args: {
+    customOptions: [
+      { value: "option1", label: "Option 1 with very very long name" },
+      { value: "option2", label: "Option 2" },
+      { value: "option3", label: "Option 3" },
+    ],
+  },
+  argTypes: {
+    customOptions: {
+      control: 'object',
+      description: 'Array of options for the combobox',
+    },
+  },
+  render: (args) => {
+    const [searchValue, setSearchValue] = useState("");
+    return <Command>
+          <CommandInput value={searchValue} onChange={(e) => {
+            setSearchValue(e.target.value);
+          }} />
+          <CommandList>
+            {args.customOptions.map((option) => (
+              <CommandOption key={option.value} value={option.value}>
+                {option.label}
+              </CommandOption>
+            ))}
+          </CommandList>
+        </Command>
+  }
+};
