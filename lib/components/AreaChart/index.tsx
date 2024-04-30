dfbda8e646be37559fe10ad224d9752efc7dbce7
pyyding <kaspar.pyyding@gmail.com>
2024-04-30 13:15:33 +0300

    make AreaChart const instead of fn

--- lib/components/AreaChart/index.tsx
+++ lib/components/AreaChart/index.tsx
@@ -8,20 +8,22 @@ import { cx } from "../common/utils.ts";
 
 export type AreaChartProps = React.ComponentProps<typeof RechartsAreaChart>;
 
-export function AreaChart({ children, className, ...rest }: AreaChartProps) {
-  return (
-    <div
-      className={cx(className, "h-80 w-full")}
-      data-testid="area-chart-wrapper"
-    >
-      <RechartsResponsiveContainer width="100%" height="100%">
-        <RechartsAreaChart
-          margin={{ top: 10, left: 10, right: 10, bottom: 10 }}
-          {...rest}
-        >
-          {children}
-        </RechartsAreaChart>
-      </RechartsResponsiveContainer>
-    </div>
-  );
-}
+export const AreaChart: React.FC<AreaChartProps> = ({
+  children,
+  className,
+  ...rest
+}) => (
+  <div
+    className={cx(className, "h-80 w-full")}
+    data-testid="area-chart-wrapper"
+  >
+    <RechartsResponsiveContainer width="100%" height="100%">
+      <RechartsAreaChart
+        margin={{ top: 10, left: 10, right: 10, bottom: 10 }}
+        {...rest}
+      >
+        {children}
+      </RechartsAreaChart>
+    </RechartsResponsiveContainer>
+  </div>
+);
