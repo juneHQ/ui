b9be571167d839fcbc4710de0c3748505ff2134a
ferrucc-io <ferruccio.balestreri@gmail.com>
2024-05-02 14:22:21 +0000

    Deploying to gh-pages from @ juneHQ/ui@c038ed270e4ea2d17ff5803c11a566dc5d9ed72b 🚀

---
+++ assets/Button.stories-CtKmCQAh.js
@@ -0,0 +1,66 @@
+import{j as p,M as H}from"./MagnifyingGlassIcon-lGZ8Xy5M.js";import{B as K}from"./index-Vr3s_2qG.js";import"./index-CsdIBAqE.js";const V={title:"Components/Button",component:K,args:{}},r={args:{children:"Default button"}},e={args:{children:"Primary",color:"purple"}},a={args:{children:"Disabled",disabled:!0}},n={args:{children:"Outline",variant:"outline",color:"yellow"}},s={args:{children:"Purple ghost",color:"purple",variant:"ghost"}},o={args:{children:"Long button text",loading:!0}},t={args:{children:"Loading",loading:!0,loadingText:"Submitting..."}},c={args:{children:"Large",size:"lg"}},i={args:{children:p.jsxs(p.Fragment,{children:[p.jsx(H,{}),"Search"]})}},l={args:{children:p.jsx(H,{}),color:"purple",size:"sm",variant:"ghost"}},d={args:{children:"Open June",variant:"ghost",color:"purple",href:"https://june.so",target:"_blank",rel:"noreferrer"}};var u,g,m;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    children: "Default button"
+  }
+}`,...(m=(g=r.parameters)==null?void 0:g.docs)==null?void 0:m.source}}};var h,x,S;e.parameters={...e.parameters,docs:{...(h=e.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    children: "Primary",
+    color: "purple"
+  }
+}`,...(S=(x=e.parameters)==null?void 0:x.docs)==null?void 0:S.source}}};var b,f,L;a.parameters={...a.parameters,docs:{...(b=a.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    children: "Disabled",
+    disabled: true
+  }
+}`,...(L=(f=a.parameters)==null?void 0:f.docs)==null?void 0:L.source}}};var y,O,v;n.parameters={...n.parameters,docs:{...(y=n.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    children: "Outline",
+    variant: "outline",
+    color: "yellow"
+  }
+}`,...(v=(O=n.parameters)==null?void 0:O.docs)==null?void 0:v.source}}};var D,P,j;s.parameters={...s.parameters,docs:{...(D=s.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    children: "Purple ghost",
+    color: "purple",
+    variant: "ghost"
+  }
+}`,...(j=(P=s.parameters)==null?void 0:P.docs)==null?void 0:j.source}}};var I,T,G;o.parameters={...o.parameters,docs:{...(I=o.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    children: "Long button text",
+    loading: true
+  }
+}`,...(G=(T=o.parameters)==null?void 0:T.docs)==null?void 0:G.source}}};var k,w,z;t.parameters={...t.parameters,docs:{...(k=t.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    children: "Loading",
+    loading: true,
+    loadingText: "Submitting..."
+  }
+}`,...(z=(w=t.parameters)==null?void 0:w.docs)==null?void 0:z.source}}};var M,_,B;c.parameters={...c.parameters,docs:{...(M=c.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    children: "Large",
+    size: "lg"
+  }
+}`,...(B=(_=c.parameters)==null?void 0:_.docs)==null?void 0:B.source}}};var A,E,J;i.parameters={...i.parameters,docs:{...(A=i.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <MagnifyingGlassIcon />
+        Search
+      </>
+  }
+}`,...(J=(E=i.parameters)==null?void 0:E.docs)==null?void 0:J.source}}};var W,Y,C;l.parameters={...l.parameters,docs:{...(W=l.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    children: <MagnifyingGlassIcon />,
+    color: "purple",
+    size: "sm",
+    variant: "ghost"
+  }
+}`,...(C=(Y=l.parameters)==null?void 0:Y.docs)==null?void 0:C.source}}};var F,R,q;d.parameters={...d.parameters,docs:{...(F=d.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    children: "Open June",
+    variant: "ghost",
+    color: "purple",
+    href: "https://june.so",
+    target: "_blank",
+    rel: "noreferrer"
+  }
+}`,...(q=(R=d.parameters)==null?void 0:R.docs)==null?void 0:q.source}}};const X=["Default","Primary","Disabled","YellowOutline","PurpleGhost","Loading","LoadingWithText","Large","IconAndText","IconOnly","Link"];export{r as Default,a as Disabled,i as IconAndText,l as IconOnly,c as Large,d as Link,o as Loading,t as LoadingWithText,e as Primary,s as PurpleGhost,n as YellowOutline,X as __namedExportsOrder,V as default};
