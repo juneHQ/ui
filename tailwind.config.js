e7381a6c3c5aa0e5d35837aeb7046fac90533b40
pyyding <kaspar.pyyding@gmail.com>
2024-05-07 15:10:02 +0300

    generate tailwind styles from classes in stories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./lib/**/*.{js,ts,jsx,tsx}"],
+  content: ["./lib/**/*.{js,ts,jsx,tsx}", "./src/stories/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       zIndex: {
