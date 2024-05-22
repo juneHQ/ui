9e0d83119aa94b3632707d5027eca455db9a9a7c
Ferruccio Balestreri <ferruccio.balestreri@gmail.com>
2024-05-22 11:29:18 +0200

    Remove deprecated components

--- lib/main.ts
+++ lib/main.ts
@@ -6,44 +6,44 @@ export { BarItem } from "./components/BarChart/BarItem";
 export type { BarItemProps } from "./components/BarChart/BarItem";
 export { XAxis } from "./components/XAxis";
 export {
-  defaultXAxisProps,
-  scatterXAxisProps,
+	defaultXAxisProps,
+	scatterXAxisProps,
 } from "./components/XAxis/constants";
 export type { XAxisProps } from "./components/XAxis";
 export { YAxis } from "./components/YAxis";
 export {
-  defaultYAxisProps,
-  scatterYAxisProps,
+	defaultYAxisProps,
+	scatterYAxisProps,
 } from "./components/YAxis/constants";
 export type { YAxisProps } from "./components/YAxis";
 export { Grid, type GridProps } from "./components/Grid";
 export {
-  defaultGridProps,
-  scatterGridProps,
+	defaultGridProps,
+	scatterGridProps,
 } from "./components/Grid/constants";
 export {
-  ChartTooltip,
-  DefaultTooltip,
-  ChartTooltipTitle,
-  ChartTooltipValue,
-  ChartTooltipFooter,
-  ChartTooltipContent,
-  type ChartTooltipProps,
+	ChartTooltip,
+	DefaultTooltip,
+	ChartTooltipTitle,
+	ChartTooltipValue,
+	ChartTooltipFooter,
+	ChartTooltipContent,
+	type ChartTooltipProps,
 } from "./components/ChartTooltip";
 export {
-  defaultTooltipProps,
-  scatterChartTooltipProps,
+	defaultTooltipProps,
+	scatterChartTooltipProps,
 } from "./components/ChartTooltip/constants";
 export { EmptyState } from "./components/EmptyState";
 export type { EmptyStateProps } from "./components/EmptyState";
 export {
-  Lifecycle,
-  Command,
-  CommandOption,
-  CommandList,
-  CommandInput,
+	Lifecycle,
+	Command,
+	CommandOption,
+	CommandList,
+	CommandInput,
 } from "./components/Command";
-export { Popover } from "./components/Popover";
+export { Popover, PopoverButton, PopoverPanel } from "./components/Popover";
 export { Button, type ButtonProps } from "./components/Button";
 export { LineChart, type LineChartProps } from "./components/LineChart";
 export { Line } from "./components/Line";
@@ -52,29 +52,29 @@ export { AreaChart, type AreaChartProps } from "./components/AreaChart";
 export { Area, type AreaProps } from "./components/Area";
 export { defaultAreaProps } from "./components/Area/constants";
 export {
-  ReferenceLine,
-  type ReferenceLineProps,
+	ReferenceLine,
+	type ReferenceLineProps,
 } from "./components/ReferenceLine";
 export {
-  defaultReferenceLineProps,
-  scatterReferenceLineYProps,
-  scatterReferenceLineXProps,
+	defaultReferenceLineProps,
+	scatterReferenceLineYProps,
+	scatterReferenceLineXProps,
 } from "./components/ReferenceLine/constants.ts";
 export { Dot } from "./components/Dot";
 export { defaultDotProps } from "./components/Dot/constants";
 export { Legend, type LegendProps } from "./components/Legend";
 export { ChartLabel, type ChartLabelProps } from "./components/ChartLabel";
 export {
-  ChartLabelText,
-  type ChartLabelTextProps,
+	ChartLabelText,
+	type ChartLabelTextProps,
 } from "./components/ChartLabel/ChartLabelText.tsx";
 export { TickText, type TickTextProps } from "./components/TickText";
 export { imagineColor } from "./components/common/utils";
 export { ChartCell, type ChartCellProps } from "./components/ChartCell";
 export { scatterChartCellProps } from "./components/ChartCell/constants";
 export {
-  ScatterChart,
-  type ScatterChartProps,
+	ScatterChart,
+	type ScatterChartProps,
 } from "./components/ScatterChart";
 export { Scatter, type ScatterProps } from "./components/Scatter";
 export { defaultScatterProps } from "./components/Scatter/constants";
